e5b408e75d5f712a6e6732120c010aa3dbed7c91
zakaria-akash <zakaria.93@yahoo.com>
2023-02-02 17:16:26 +0600

    convert all html pages into php files; separate header and footer sections from webpages

---
+++ about.php
@@ -0,0 +1,10 @@
+<?php include "layout/header.php"; ?>
+<h2>About</h2>
+
+<p class="text-center">
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore
+    impedit totam porro iure reiciendis autem possimus sapiente, optio,
+    exercitationem ipsum assumenda mollitia, recusandae expedita culpa
+    ratione voluptatem esse quos quam?
+</p>
+<?php include "layout/footer.php"; ?>
